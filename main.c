2885204dd994963a9e50bdb1819682ba26f8a4eb
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-04-19 15:45:19 +0000

    adding main.c

---
+++ main.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+int main(__attribute__((unused)) int ac ,__attribute__((unused))char **argv)
+{
+        char *prompt = "$";
+        char *lineptr = NULL;
+        size_t n = 0;
+        ssize_t char_read;
+
+                while(1)
+                {
+                        printf("%s", prompt);
+                        char_read = getline(&lineptr, &n, stdin);
+
+                        if (char_read == -1)
+                        {
+                                printf("Exit\n");
+                                return(-1);
+                        }
+
+                        printf("%s\n", lineptr);
+
+                        free(lineptr);
+                }
+
+        return (0);
+}
