5dcb45fa9b9b24f36af88ab7bf758943a6f8fa91
Xai Betancourt <betancourtxavier77@gmail.com>
2023-04-24 14:13:14 +0000

    uploading auxiliary function for the execution function

---
+++ get_directions.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+char *directions(char *cmnd)
+{
+	char *path, *path_copy, *path_token, *file_path;
+	int cmnd_len, dir_len;
+	struct stat buffer;
+
+	path = getenv("PATH");
+	if (path)
+	{
+		path_copy = strdup(path);
+		cmnd_len = strlen(cmnd);
+		path_token = strtok(path_copy, ":");
+		while (path_token != NULL)
+		{
+			dir_len = strlen(path_token);
+			file_path = malloc(cmnd_len + dir_len + 2);
+			strcpy(file_path, path_token);
+			strcat(file_path, "/");
+			strcat(file_path, cmnd);
+			strcat(file_path, "\0");
+			if (stat(file_path, &buffer) == 0)
+			{
+				free(path_copy);
+				return (file_path);
+			}
+			else
+			{
+				free(file_path);
+				path_token = strtok(NULL, ":");
+			}
+		}
+		free(path_copy);
+		if (stat(cmnd, &buffer) == 0)
+			return (cmnd);
+		return (NULL);
+	}
+	return (NULL);
+}
