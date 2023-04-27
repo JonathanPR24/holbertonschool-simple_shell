8a8f2bca80985cc369db2cc5a40ba9c33561105a
JonathanPR24 <jonathanperez604@gmail.com>
2023-04-27 15:53:02 +0000

    pass betty

--- free_array.c
+++ free_array.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
+/**
+ * free_array - deallocate memory used by an array of strings
+ *
+ * @token: pointer to an array of null-terminated strings
+ *
+ * Return: none
+ */
 void free_array(char **token)
 {
 	int i = 0;
