dd21db2bcab6eae1d95c110a6f64b0d688a3eaf1
Jonathan Perez <jonathanperez604@gmail.com>
2023-04-26 20:54:47 -0400

    Update enviroment.c

--- enviroment.c
+++ enviroment.c
@@ -3,14 +3,15 @@
 /**
  * print_env - print current enviroment for simple shell
  */
+
 void print_env(void)
 {
-	extern char **environ;
-	char **env = environ;
+        extern char **environ;
+        char **env = environ;
 
-	while (*env)
-	{
-		printf("%s\n", *env);
-		env++;
-	}
+        while (*env)
+        {
+                printf("%s\n", *env);
+                env++;
+        }
 }
