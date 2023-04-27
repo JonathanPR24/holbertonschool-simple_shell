e32b8944de8ac1d88c195b787b1b3fa9cbde903c
Jonathan Perez <jonathanperez604@gmail.com>
2023-04-26 20:49:14 -0400

    Update shell.h

--- shell.h
+++ shell.h
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -15,7 +14,7 @@ char **parser(char *token, char *delim);
 int exec(char **argv);
 char *directions(char *cmnd);
 void free_array(char **token);
-void print_env();
-void exec_exit(void);
+extern void exec_exit(void);
+void print_env(void);
 
 #endif
