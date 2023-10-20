1873ed3c98efb5c01afd872f29881792a272ec92
Xai_Betancourt~ <betancourtxavier77@gmail.com>
2023-10-20 16:03:00 -0400

    Adding clarifying comments to environment

--- enviroment.c
+++ enviroment.c
@@ -1,17 +1,16 @@
-#include "shell.h"
+#include "shell.h"  // Include the "shell.h" header file for function prototypes and declarations.
 
 /**
- * print_env - print current enviroment for simple shell
+ * print_env - print current environment for a simple shell
  * @environ: env variable
  **/
-
 void print_env(char **environ)
 {
-	char **env = environ;
+	char **env = environ;  // Initialize a pointer 'env' to the provided 'environ' variable.
 
-	while (*env)
+	while (*env)  // Iterate through the environment variables until a NULL pointer is encountered.
 	{
-		printf("%s\n", *env);
-		env++;
+		printf("%s\n", *env);  // Print the current environment variable.
+		env++;  // Move to the next environment variable.
 	}
 }
